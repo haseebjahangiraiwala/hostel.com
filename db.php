5a462e93acec19a8b598a7dc6de5ea859403b9a5
Haseeb Jahangir AI Wala <haseebjahangiraiwala@gmail.com>
2025-09-01 17:24:18 +0500

    Create db.php

---
+++ db.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "uulevslgtrnau";
+$pass = "ld4dy42tkorz";
+$dbname = "dbvd5cigccjb6w";
+ 
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+ 
+if (!$conn) {
+    die("Database Connection Failed: " . mysqli_connect_error());
+}
+?>
+ 
